7336c4fb099d424decbbfe8e4999fc1a8f21bf7e
JakubL <jakubxleszczewski@gmail.com>
2022-09-23 01:45:15 +0200

    add findOne user

---
+++ src/types/user/user-response.ts
@@ -0,0 +1,6 @@
+import { UserSaveResponseData } from './user';
+
+export type GetUserResponse = UserSaveResponseData;
+export type CreateUserResponse = UserSaveResponseData;
+export type UpdateUserResponse = UserSaveResponseData;
+export type DeleteUserResponse = UserSaveResponseData;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { FindOptionsWhere } from 'typeorm';
+import { GetUserResponse, UserSaveResponseData } from '../types';
 
 @Injectable()
 export class UserService {
+  async getUser(where: FindOptionsWhere<User>): Promise<User> {
+    return User.findOne({ where });
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +23,13 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<GetUserResponse> {
+    if (!id) throw new BadRequestException();
+
+    const user = await this.getUser({ id });
+    if (!user) throw new NotFoundException();
+
+    return this.filter(user);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
@@ -22,5 +38,11 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  filter(userEntity: User): UserSaveResponseData {
+    const { jwtId, hashPwd, photoFn, ...userResponse } = userEntity;
+
+    return { ...userResponse, avatar: `/user/photo/${userResponse.id}` };
   }
 }
